432606fd975608aeb5489b96b53d8dd2fcb2ab94
Jeff <xorgus1234@gmail.com>
2021-03-10 23:39:35 +0900

    boj: 2947
    구현, 시뮬레이션

--- baekjoon/0310_boj_2947.swift
+++ baekjoon/0310_boj_2947.swift
@@ -4,5 +4,16 @@
 //
 //  Created by Jeff on 2021/03/10.
 //
+//  나무조각
+//  https://www.acmicpc.net/problem/2947
 
-import Foundation
+var wood = readLine()!.split(separator: " ").map{ Int(String($0))! }
+var startPos = 0
+let target = [1,2,3,4,5]
+while wood != target {
+    if wood[startPos] > wood[startPos+1] {
+        wood.swapAt(startPos, startPos+1)
+        print(wood.map{ String($0)}.joined(separator: " "))
+    }
+    startPos = startPos == 3 ? 0 : startPos+1
+}
