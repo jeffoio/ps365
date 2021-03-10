f1e41475ee05e3a9e43154320dd4d4abdf94439f
Jeff <xorgus1234@gmail.com>
2021-03-10 23:48:22 +0900

    boj: 12605
    자료구조, 스택

--- baekjoon/0310_boj_12605.swift
+++ baekjoon/0310_boj_12605.swift
@@ -4,5 +4,14 @@
 //
 //  Created by Jeff on 2021/03/10.
 //
+//  단어순서 뒤집기
+//  https://www.acmicpc.net/problem/12605
 
-import Foundation
+let testCount = Int(readLine()!)!
+
+for caseNum in 0..<testCount {
+    var input = readLine()!.split(separator: " ")
+    input.reverse()
+    
+    print("Case #\(caseNum+1): \(input.joined(separator: " "))")
+}
