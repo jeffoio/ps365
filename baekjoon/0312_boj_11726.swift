acb7bdb53a1b83d9adc193713f0954816123ae69
Jeff <xorgus1234@gmail.com>
2021-03-12 23:53:46 +0900

    boj: 11726
    DP

--- baekjoon/0312_boj_11726.swift
+++ baekjoon/0312_boj_11726.swift
@@ -4,5 +4,31 @@
 //
 //  Created by Jeff on 2021/03/12.
 //
+//  2xn 타일링
+//  https://www.acmicpc.net/problem/11726
 
-import Foundation
+// 다이나믹 - 문제를 나눌때 단계 1개 + 나머지 전체로 나누기
+let n = Int(readLine()!)!
+var d = Array(repeating: 0, count: n+1)
+
+ //bottom to top
+d[0] = 1
+d[1] = 1
+
+for i in 2...n {
+    d[i] = (d[i-1] + d[i-2])%10007
+}
+
+print(d[n])
+
+// top to bottom
+func tile(num: Int) -> Int {
+    if num == 1 { return 1 }
+    if num == 2 { return 2 }
+
+    if d[num] != 0 { return d[num] }
+    d[num] = (tile(num: num-1) % 10007) + (tile(num: num-2) % 10007)
+
+    return d[num] % 10007
+}
+
