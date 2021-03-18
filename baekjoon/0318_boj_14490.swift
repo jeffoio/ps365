3819714460db37cde690419720a551ae0b3863b4
Jeff <xorgus1234@gmail.com>
2021-03-18 16:33:44 +0900

    boj: 14490
    문자열

--- baekjoon/0318_boj_14490.swift
+++ baekjoon/0318_boj_14490.swift
@@ -4,5 +4,17 @@
 //
 //  Created by Jeff on 2021/03/18.
 //
+//  백대열
+//  
+var input = readLine()!.split(separator: ":").map{ Int(String($0))!}
+var g = gcd(a: input[0],b: input[1])
 
-import Foundation
+print("\(input[0]/g):\(input[1]/g)")
+
+
+func gcd(a: Int, b:Int) -> Int {
+    if b == 0 { return a}
+    else {
+        return gcd(a: b, b: a%b)
+    }
+}
