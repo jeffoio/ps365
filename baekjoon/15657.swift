46e0f42c1ec0347ac6d913d7e6c937250c01caa2
Jeff <xorgus1234@gmail.com>
2021-04-02 17:26:55 +0900

    boj: 15657
    백트래킹

---
+++ baekjoon/15657.swift
@@ -0,0 +1,26 @@
+//
+//  15657.swift
+//  codeingtest
+//
+//  Created by Jeff on 2021/04/02.
+//
+//  N과 M (8)
+//  https://www.acmicpc.net/problem/15657
+
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = input[0], m = input[1]
+var numbers = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
+
+func backtracking(idx: Int, arr: [Int]) {
+    if idx == m {
+        print(arr.map{String($0)}.joined(separator:" "))
+        return
+    }
+    
+    for i in 0..<n {
+        if !arr.isEmpty, arr[idx-1] > numbers[i] { continue }
+        backtracking(idx: idx+1, arr: arr + [numbers[i]])
+    }
+}
+
+backtracking(idx: 0, arr: [Int]())
